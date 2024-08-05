89bf451ec6a4fb17576f51604b21fe73b0c7a5a9
bots-hosting-account <155102703+bots-hosting-account@users.noreply.github.com>
2024-08-05 14:05:17 -0400

    Stop +features from modifying other embeds

--- bot/features.py
+++ bot/features.py
@@ -1,16 +1,22 @@
+import random
+
 from discord_ui import SelectMenu, SelectOption
 from asyncio.exceptions import TimeoutError as AETimeoutError
+
 from features_constants import features_basic, feature_texts, plushelp_msg
+
 
 async def run(message, client, recurring=False):
   global features_msg, feature_options
+
+  pre = random.randint(1, 1000000)
   feature_options = [
-    SelectOption(label="Basic Commands", value="cmds_basic"),
-    SelectOption(label="General Commands", value="cmds_general"),
-    SelectOption(label="Text Mangling Commands", value="cmds_text"),
-    SelectOption(label="Text Generating Commands", value="cmds_textgen"),
-    SelectOption(label="Currency-Related Commands", value="cmds_currency"),
-    SelectOption(label="Channel-Related Commands", value="cmds_chans")
+    SelectOption(label="Basic Commands", value=f"{pre}cmds_basic"),
+    SelectOption(label="General Commands", value=f"{pre}cmds_general"),
+    SelectOption(label="Text Mangling Commands", value=f"{pre}cmds_text"),
+    SelectOption(label="Text Generation Commands", value=f"{pre}cmds_textgen"),
+    SelectOption(label="Currency-Related Commands", value=f"{pre}cmds_currency"),
+    SelectOption(label="Channel-Related Commands", value=f"{pre}cmds_chans")
   ]
   
   sm = SelectMenu(
