539582a1bd4771aa4c86c7aaad787678dde07c14
bots-hosting-account <155102703+bots-hosting-account@users.noreply.github.com>
2025-01-04 23:02:14 -0500

    Dictionary hints

--- bot/eoo_words.py
+++ bot/eoo_words.py
@@ -1,10 +1,15 @@
 from discord import Embed
 import csv
 
-def make_word(word, english, pos, ipa, etymology, notes):
+def make_word(word, english, hint, pos, ipa, etymology, notes):
   return {
-    "word": word, "english": english, "pos": ("ADJ" if pos == "ADV" else pos),
-    "ipa": ipa, "etymology": etymology, "notes": notes
+    "word": word,
+    "english": english,
+    "hint": hint,
+    "pos": ("ADJ" if pos == "ADV" else pos),
+    "ipa": ipa,
+    "etymology": etymology,
+    "notes": notes
   }
 
 def get_words(filename):
@@ -15,39 +20,49 @@ def get_words(filename):
       words.append(make_word(*row))
   return words
 
-words = get_words("assets/eoo_words.csv")
-
 def search(query, search_eoo=False):
+  words = get_words("assets/eoo_words.csv")
+
   search_field = "word" if search_eoo else "english"
-  
   dict_query = query.strip().lower()
-  results = [word for word in words if word[search_field].lower() == dict_query]
-  if search_eoo:
-    results = results[:1]
-  
+
+  results = []
+  seen = set()
+  for word in words:
+    if word[search_field].lower() == dict_query:
+      seen_key = (word["word"], word["pos"])
+      if seen_key not in seen:
+        results.append(word)
+        seen.add(seen_key)
+
   e = Embed(title=f'Search results for "{query}"', color=0x33aa33)
-  
+
   if len(results) == 0:
     e.add_field(name="No results", value="No words matched your search query.", inline=False)
   else:
     for result in results:
       homonyms = [word for word in words if word["word"] == result["word"] and word["pos"] == result["pos"]]
-      
+
       value = ""
       for i, word in enumerate(homonyms):
-        bold = "**" if word["english"].lower() == dict_query else ""
+        if not search_eoo and word["english"].lower() == dict_query:
+          bold = "**"
+        else:
+          bold = ""
         sp = "\n\u200b\t\u200b\t\u200b\t\u200b\t\u200b    "
         value += "\u200b\t" + str(i + 1) + ". " + bold + word["english"] + bold
+        if len(word["hint"]) > 0:
+          value += " (" + word["hint"] + ")"
         if len(word["etymology"]) > 0:
           value += sp + word["etymology"].replace("\n", sp)
         if len(word["notes"]) > 0:
           value += sp + word["notes"].replace("\n", sp)
         value += "\n"
-      
+
       pos = word["pos"].lower()
       if pos == "adj":
         pos = "adj. or adv"
       name = word["word"] + " *" + pos + ".*"
       e.add_field(name=name, value=value[:-1], inline=False)
-  
+
   return e
